c6419a276ac2c79a83c7fe1e2059f39e63b20dd1
nadershbib <nader_shbib@hotmail.com>
2026-07-11 11:38:35 -0700

    one solved

---
+++ 6kyu/YourOrdersPlease.py
@@ -0,0 +1,21 @@
+# https://www.codewars.com/kata/55c45be3b2079eccff00010f/train/python
+
+def order(sentence):
+
+    dict = {}
+
+    for word in sentence.split():
+        for c in word:
+            if c.isdigit():
+                dict[int(c)] = word
+    return " ".join(dict[key] for key in sorted(dict))
+    
+
+
+print(order("is2 Thi1s T4est 3a"))
+
+
+
+
+
+
